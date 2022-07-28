e92385e8833a405e5d996120a9d9eba37176dfc3
ilham-zukri <zukri.ilham@gmail.com>
2022-07-28 13:35:03 +0700

    Adding PUT page and PUT method, Also adding copyright text

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -2,6 +2,7 @@ import 'package:consume_api/delete_page.dart';
 import 'package:consume_api/get_page.dart';
 import 'package:consume_api/patch_page.dart';
 import 'package:consume_api/post_page.dart';
+import 'package:consume_api/put_page.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -23,87 +24,102 @@ class _MainPageState extends State<MainPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: ListView(children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                height: height / 10,
-                width: width,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
+        child: ListView(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: height / 10,
+                  width: width,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return const GetPage();
+                          },
+                        ),
+                      );
+                    },
+                    child: Text('GET'),
+                  ),
+                ),
+                Container(
+                  height: height / 10,
+                  width: width,
+                  margin: EdgeInsets.only(top: 8),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return const PostPage();
+                          },
+                        ),
+                      );
+                    },
+                    child: Text('POST'),
+                  ),
+                ),
+                Container(
+                  height: height / 10,
+                  width: width,
+                  margin: EdgeInsets.only(top: 8),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(
                         builder: (context) {
-                          return const GetPage();
+                          return const PatchPage();
                         },
-                      ),
-                    );
-                  },
-                  child: Text('GET'),
+                      ));
+                    },
+                    child: Text('PATCH'),
+                  ),
                 ),
-              ),
-              Container(
-                height: height / 10,
-                width: width,
-                margin: EdgeInsets.only(top: 8),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
+                Container(
+                  height: height / 10,
+                  width: width,
+                  margin: EdgeInsets.only(top: 8),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(
                         builder: (context) {
-                          return const PostPage();
+                          return DeletePage();
                         },
-                      ),
-                    );
-                  },
-                  child: Text('POST'),
+                      ));
+                    },
+                    child: Text('DELETE'),
+                  ),
                 ),
-              ),
-              Container(
-                height: height / 10,
-                width: width,
-                margin: EdgeInsets.only(top: 8),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(
-                      builder: (context) {
-                        return const PatchPage();
-                      },
-                    ));
-                  },
-                  child: Text('PATCH'),
+                Container(
+                  height: height / 10,
+                  width: width,
+                  margin: EdgeInsets.only(top: 8),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const PutPage(),
+                        ),
+                      );
+                    },
+                    child: Text('PUT'),
+                  ),
                 ),
+              ],
+            ),
+            Center(
+              child: Container(
+                margin: EdgeInsets.only(top: 16),
+                child: Text('2022 \u00a9 Aulia Ilham Zukri - 20190801368')
               ),
-              Container(
-                height: height / 10,
-                width: width,
-                margin: EdgeInsets.only(top: 8),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(
-                      builder: (context) {
-                        return DeletePage();
-                      },
-                    ));
-                  },
-                  child: Text('DELETE'),
-                ),
-              ),
-              Container(
-                height: height / 10,
-                width: width,
-                margin: EdgeInsets.only(top: 8),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text('PUT'),
-                ),
-              ),
-            ],
-          ),
-        ]),
+            )
+          ],
+        ),
       ),
     );
   }
